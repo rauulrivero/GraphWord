204193667781e03ffecff7317b324285f0667289
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-01-15 19:57:28 +0000

    Delete test.graphify.py

--- test.graphify.py
+++
@@ -1,7 +0,0 @@
-# Simplified Graphify Test
-from graphify.src.utils.text_processor import TextProcessor
-
-def test_text_processor():
-    processor = TextProcessor("hello world hello")
-    word_freq = processor.get_word_frequency()
-    assert word_freq == {"hello": 2, "world": 1}
