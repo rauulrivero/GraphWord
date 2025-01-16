399188ad23938f19651e774e9ea51bd89909da1a
rauul <rauulriveroo@gmail.com>
2025-01-16 16:33:10 +0000

    Changes to be committed: 	modified:   api/src/__init__.py

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,12 +1,5 @@
 from flask import Flask
-<<<<<<< HEAD
 from config.config import Config, ProductionConfig, DevelopmentConfig, TestingConfig
-=======
-from api.config.config import Config, ProductionConfig, DevelopmentConfig, TestingConfig
-from api.src.utils.file_manager import FileManager
-from database.graph import WordGraph
-from src.aws.s3_manager import S3Manager
->>>>>>> 0839766c38b3ad09a484e7cff885d877dbf9bb15
 from dotenv import load_dotenv
 
 
