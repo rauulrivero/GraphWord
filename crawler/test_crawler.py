5115ce644a46c07a76ebd5d57c7b57ecbbf20aea
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-01-16 18:56:03 +0000

    Update test_crawler.py

--- crawler/test_crawler.py
+++ crawler/test_crawler.py
@@ -61,24 +61,24 @@ def test_controller_run():
     except Exception as e:
         print(f"[FAILED] run con book_ids={book_ids}: {e}")
 
-def test_lambda_handler():
-    """
-    Prueba la función lambda_handler simulando un evento.
-    """
-    from lambda_function import lambda_handler
+# def test_lambda_handler():
+#     """
+#     Prueba la función lambda_handler simulando un evento.
+#     """
+#     from lambda_function import lambda_handler
 
-    event = {
-        "book_ids": [1342, 11, 84]
-    }
+#     event = {
+#         "book_ids": [1342, 11, 84]
+#     }
 
-    context = {}  # Simulación de contexto
+#     context = {}  # Simulación de contexto
 
-    try:
-        response = lambda_handler(event, context)
-        assert response['statusCode'] == 200, "El estado HTTP no es 200."
-        print(f"[PASSED] lambda_handler con event={event}")
-    except Exception as e:
-        print(f"[FAILED] lambda_handler con event={event}: {e}")
+#     try:
+#         response = lambda_handler(event, context)
+#         assert response['statusCode'] == 200, "El estado HTTP no es 200."
+#         print(f"[PASSED] lambda_handler con event={event}")
+#     except Exception as e:
+#         print(f"[FAILED] lambda_handler con event={event}: {e}")
 
 if __name__ == "__main__":
     print("Iniciando pruebas del crawler...")
