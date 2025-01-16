0f917786fdb62851ab3908c074894e396aa7dad6
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-01-16 18:09:44 +0000

    Update test_crawler.py

--- crawler/test_crawler.py
+++ crawler/test_crawler.py
@@ -30,41 +30,6 @@ def test_fetch_book(mock_get):
     mock_get.assert_called_once_with(f'https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt')
     assert content == "Contenido del libro"
 
-@patch('src.downloader.http_handler.fetch_book')
-def test_process_books(mock_fetch_book, controller, mock_s3_manager):
-    # Configurar el mock de fetch_book
-    book_id = 1234
-    book_content = "Contenido de prueba"
-    mock_fetch_book.return_value = book_content
-
-    # Llamar al método a probar
-    controller.process_books(book_id)
-
-    # Verificaciones
-    mock_fetch_book.assert_called_once_with(book_id)
-    mock_s3_manager.put_object.assert_called_once_with(
-        Body=book_content,
-        Bucket='test-datalake',
-        Key=f'{book_id}.txt',
-        ContentType='text/plain'
-    )
-
-@patch('src.controller.Controller.process_books')
-def test_run(mock_process_books, controller):
-    # Configurar el mock del método process_books
-    mock_process_books.return_value = None
-
-    start_id = 1000
-    n_books = 5
-
-    # Llamar al método a probar
-    controller.run(start_id, n_books)
-
-    # Verificaciones
-    assert mock_process_books.call_count == n_books
-    for i in range(start_id, start_id + n_books):
-        mock_process_books.assert_any_call(i)
-
 @patch('src.aws.s3_manager.S3Manager.upload_text_file')
 def test_upload_text_file(mock_upload, mock_s3_manager):
     # Configurar el mock de upload_text_file
