44d9d68a836878c06bc128ce2ad5c9a07d8fb5f9
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-01-17 16:56:11 +0000

    Update test_crawler.py

--- crawler/test_crawler.py
+++ crawler/test_crawler.py
@@ -30,25 +30,6 @@ def test_s3_upload():
     except Exception as e:
         print(f"[FAILED] upload_text_file para {s3_key}: {e}")
 
-def test_lambda_handler():
-    """
-    Prueba la función lambda_handler simulando un evento.
-    """
-    from lambda_function import lambda_handler
-
-    event = {
-        "book_ids": [1342, 11, 84]
-    }
-
-    context = {}  # Simulación de contexto
-
-    try:
-        response = lambda_handler(event, context)
-        assert response['statusCode'] == 200, "El estado HTTP no es 200."
-        print(f"[PASSED] lambda_handler con event={event}")
-    except Exception as e:
-        print(f"[FAILED] lambda_handler con event={event}: {e}")
-
 
 if __name__ == "__main__":
     print("Iniciando pruebas del crawler...")
@@ -56,6 +37,5 @@ if __name__ == "__main__":
     # Probar cada componente individualmente
     test_fetch_book()
     test_s3_upload()
-    test_lambda_handler()
 
     print("Pruebas completadas.")
