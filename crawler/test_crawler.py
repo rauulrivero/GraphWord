b70596570887e760e7cd73aa0c0aa85b5890b41b
Pablo Medina <113461293+ppablomedina@users.noreply.github.com>
2025-01-17 17:40:55 +0000

    Update test_crawler.py

--- crawler/test_crawler.py
+++ crawler/test_crawler.py
@@ -3,25 +3,19 @@ from crawler.src.downloader.http_handler import fetch_book
 import requests
 
 def test_fetch_book():
-    """
-    Prueba la función fetch_book para descargar un libro.
-    """
-    test_book_id = 1342  # Ejemplo: Pride and Prejudice
+    test_book_id = 1342
     try:
         content = fetch_book(test_book_id)
-        assert content.startswith("The Project Gutenberg EBook"), "El contenido del libro no es válido."
-        print(f"[PASSED] fetch_book con book_id={test_book_id}")
+        assert content.startswith("The Project Gutenberg EBook"), "The content of the book is invalid."
+        print(f"[PASSED] fetch_book with book_id={test_book_id}")
     except Exception as e:
         print(f"[FAILED] fetch_book con book_id={test_book_id}: {e}")
 
 def test_s3_upload():
-    """
-    Prueba la carga de un archivo de texto a S3.
-    """
     bucket_name = "test-datalake"
     s3_key = "test_book.txt"
-    content = "Este es un contenido de prueba."
-
+    content = "This is test content."
+    
     s3_manager = S3Manager(region_name="us-east-1")
 
     try:
@@ -32,10 +26,6 @@ def test_s3_upload():
 
 
 if __name__ == "__main__":
-    print("Iniciando pruebas del crawler...")
 
-    # Probar cada componente individualmente
     test_fetch_book()
     test_s3_upload()
-
-    print("Pruebas completadas.")
