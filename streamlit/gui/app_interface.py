c5b2993aef39b16fa0281ad53128272848a61560
rauul <rauulriveroo@gmail.com>
2025-01-17 13:05:59 +0000

    modified:   streamlit/gui/app_interface.py

--- streamlit/gui/app_interface.py
+++ streamlit/gui/app_interface.py
@@ -9,10 +9,15 @@ class GraphVisualizer:
         """Controla la interfaz de Streamlit"""
         st.title("Visualización de Grafos")
 
+        # Configurar la URL base de la API como una variable de estado
+        if "api_base_url" not in st.session_state:
+            st.session_state.api_base_url = self.api_handler.api_base_url
+
         st.sidebar.title("Introduce la URL de la API")
-        api_base_url = st.sidebar.text_input("URL de la API", value=self.api_handler.api_base_url)
+        api_base_url = st.sidebar.text_input("URL de la API", value=st.session_state.api_base_url)
 
         if st.sidebar.button("Actualizar URL"):
+            st.session_state.api_base_url = api_base_url
             self.api_handler.update_base_url(api_base_url)
             st.sidebar.success(f"URL de la API actualizada a: {api_base_url}")
 
